64b6e1122c8b5612b2b6c992459f291c9159dae0
Sen VU <548068@student.saxion.nl>
2024-02-13 14:20:22 +0100

    added sound and controller handler

---
+++ GXPEngine/ControlerHandler.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO.Ports;
+using System.Runtime.ConstrainedExecution;
+public class ControlerHandler
+{
+    float yaw;
+    float pitch;
+    float roll;
+
+    public ControlerHandler(bool controlerActive)
+    {
+        if (!controlerActive) { return; }
+        SerialPort port = new SerialPort();
+        port.PortName = "COM4";
+        port.BaudRate = 57600;
+        port.RtsEnable = true;
+        port.DtrEnable = true;
+        port.Open();
+        while (true)
+        {
+            string line = port.ReadLine();
+            if (line != "")
+            {
+                Console.WriteLine("Read from port: " + line);
+                String[] values = line.Split(',');
+
+                yaw = float.Parse(values[0]);
+                pitch = float.Parse(values[1]);
+                roll = float.Parse(values[2]);
+
+                Console.WriteLine("Set Yaw:" + yaw);
+                Console.WriteLine("Set Pitch:" + pitch);
+                Console.WriteLine("Set Roll:" + roll);
+            } else { Console.WriteLine("cant read data from port"); }
+        }
+    }
+
+    public float getYaw() {  return yaw; }
+    public float getPitch() { return pitch; }
+    public float getRoll() { return roll; }
+
+}
+/*
+ * 
+*/
---
+++ GXPEngine/SoundHandler.cs
@@ -0,0 +1,40 @@
+﻿using GXPEngine;
+using GXPEngine.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class SoundHandler
+{
+    
+
+    public static SoundHandler TESTSOUND = new SoundHandler("", 1, 0);
+
+    Sound storedSound;
+    float defaultVolume;
+    uint defaultChanel;
+
+    public SoundHandler(string fileName, float defaultVolume, uint defaultChanel)
+    {
+        storedSound = new Sound(fileName);
+        this.defaultVolume = defaultVolume;
+        this.defaultChanel = defaultChanel;
+    }
+
+    public void play()
+    {
+        play(defaultVolume);
+    }
+
+    public void play(float volume)
+    {
+        play(volume, defaultChanel);
+    }
+
+    public void play(float volume, uint chanel)
+    {
+        storedSound.Play(false, chanel, volume, 0);
+    }
+}
