77faa232eba16ab88630f285bee2947e97aba564
Aleksandar <549190@student.saxion.nl>
2024-03-01 01:34:48 +0100

    Added grenade animation

---
+++ GXPEngine/handlers/GrenadeExplosion.cs
@@ -0,0 +1,32 @@
+﻿using GXPEngine;
+
+public class GrenadeExplosion : GameObject
+{
+    private int counter;
+    private int frame;
+
+    private AnimationSprite grenadeAnimationSprite;
+
+    public GrenadeExplosion(float x, float y)
+    {
+        SetXY(x, y);
+        grenadeAnimationSprite = new AnimationSprite("assets/sprites/UI/grenade explosion.png", 8, 6);
+        AddChild(grenadeAnimationSprite);
+    }
+
+    public void Update()
+    {
+        if (counter > 1)
+        {
+            counter = 0;
+            if (frame == grenadeAnimationSprite.frameCount)
+            {
+                LateDestroy();
+                frame = 0;
+            }
+            grenadeAnimationSprite.SetFrame(frame);
+            frame++;
+        }
+        counter++;
+    }
+}
